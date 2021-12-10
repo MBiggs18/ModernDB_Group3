da10917707c57e299c3823f49bbf7f774d68ac4b
MBiggs18 <mikayla-biggs@uiowa.edu>
2021-12-10 16:34:52 -0600

    Cleaned up code, added basic neo4j query, connection to neo4j  cluster

--- models.py
+++ models.py
@@ -4,7 +4,7 @@ import redis
 
 
 class Neo4jModel:
-    def __init__(self, url='bolt://localhost:7687', user='moderndb', password='pwd123'):
+    def __init__(self, url='neo4j+s://0c0676db.databases.neo4j.io', user='neo4j', password='Xqn4jBlgR_f9x0FVYBGrtPUEX4bp96WkZaf-D5WCeo0'):
         self.neo4jUrl = url;
         self.neo4jDriver = GraphDatabase.driver(url, auth=(user, password))
         print("Connected to {0} Neo4j Database...".format(url))
@@ -13,35 +13,17 @@ class Neo4jModel:
         self.neo4jDriver.close()
         print("Closing Neo4j Database...")
         
-    #def query1(self, tx):
-        #X = input("Please enter your userId (0-600):\n")
-        
-        # while(int(X) not in range(0,601)):
-        #     X = input("User Id must be between 0-600, try again:")
-        
-        # result = tx.run("Match (n:User) WHERE n.userId={0} RETURN n.userId, n.name".format(X))
-        # for record in result:
-        #     self.username=record.get('n.name')
-        #     self.userid=record.get('n.userId')
-       
-        # if self.userid != None:
-        #     if self.username != None:
-        #         return "Welcome back, " + str(self.username) + "!" 
-            
-        #     self.username=input("Welcome user {0}, please enter a user name:\n".format(self.userid))
-        #     # result = tx.run("MATCH(n:User) WHERE n.userId={0} SET n.name='{1}' RETURN n.userId, n.name".format(str(self.userid),str(self.username)))
-            
-        #     for record in result:
-        #         self.username=record.get('n.name')
-        #         self.userid=record.get('n.userId')
-        #     return "Welcome {0}".format(self.username)
-        
-        #return "user {}...".format(X)
         
     def print_result1(self):
         with self.neo4jDriver.session() as session:
             greeting = session.write_transaction(self.query1)
             print(greeting)
+
+    
+    def query1(self, tx):
+        result = tx.run("MATCH (c:Customer) "
+                        "RETURN c.customerId LIMIT 1")
+        return result.single()[0]
 
 
 class MongoModel:
@@ -55,7 +37,8 @@ class MongoModel:
         print("Closing Mongo Database...")
         
     #Search for top 5 vendors based on keywords
-    def searchtext ():
+    def searchtext (client):
+        client = client.mongoClient
         db = client.trydb
         userinput = input("Please enter the terms you would like to search with: \n")
     
@@ -72,7 +55,8 @@ class MongoModel:
         return()
     
     #Search for top 5 vendors based on keywords
-    def searchvendor ():
+    def searchvendor(client):
+        client = client.mongoClient
         db = client.trydb
         long = float(input("Please enter your longitude: \n"))
         lat = float(input("Please enter your latitude: \n"))
@@ -85,24 +69,4 @@ class MongoModel:
             print("\n")
         
         return()
-    
-    #def query1(self):
-        #lat = input("Please enter latitude:\n").strip()
-        #long = input("Please enter longitude:\n").strip()
-        #coords = [float(long), float(lat)]
-        #print("Coordinated entered:",coords)
-        # pipeline = [{ '$geoNear': { 'near': { '$geometry': { 'type': "Point", 'coordinates': coords }} , 'distanceField': "distance", 'key': "location" } }, { "$limit": 3 }, {'$project':{'_id':0, 'name':1, 'distance':1, 'address':1, 'city':1, 'state':1, 'stars':1, 'categories':1, 'review_count':1}}]
-        # query = self.businesses.aggregate(pipeline)
-        # print("Results:")
-        # pprint(list(query))
-        #return
-    
-    #def query2(db):
-        #terms = input("Please enter search terms separated by a space\n")
-        #print("Search terms entered: ",terms)
-        # pipeline = [{'$match': {'$text': {'$search': terms}}},{'$group':{'_id':{'name':"$name",'city':"$city",'state':"$state",'categories':"$categories", 'stars':"$stars", 'review_count':'$review_count','score':{'$meta':"textScore"}}}}, {'$sort': {"_id.score":-1}},{'$limit':5}]
-        # query = self.businesses.aggregate(pipeline)
-        # print("Results:")
-        # pprint(list(query))
-        #return
 
