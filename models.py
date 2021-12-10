ac6bb6d7ac7ba24172f73ce0a9d9ea9f77d70fea
eaanyimadu <86034604+eaanyimadu@users.noreply.github.com>
2021-12-09 20:59:53 -0600

    Update models.py

--- models.py
+++ models.py
@@ -76,7 +76,7 @@ class MongoModel:
         db = client.trydb
         long = float(input("Please enter your longitude: \n"))
         lat = float(input("Please enter your latitude: \n"))
-        myquery = db.vendors.find({"location": 
+        myquery = db.vendors.find({"geoloc": 
             {"$near": {"$geometry": {"type": "Point", "coordinates": [long, lat]}}}},
             {'_id': 0, "name": 1, "address": 1, "city": 1, "state": 1, "stars": 1, "categories": 1, "review_count": 1}).limit(5)
         for doc in myquery:
